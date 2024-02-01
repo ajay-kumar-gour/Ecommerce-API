3ec355cbf8e26ce2b6b7fbc5e3e753d24d0d49fa
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-01 23:24:24 +0530

    created a UserSchema and its Model

---
+++ Model/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("moongose");
+
+const userSchema = new mongoose.Schema({
+  Firstname: {
+    type: String,
+    required: true,
+  },
+  Lastname: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
