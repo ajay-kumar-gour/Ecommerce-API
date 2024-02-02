61083ad556b31467c3db366ab6ad38cd7111e042
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-02 11:51:30 +0530

    Implemeneted TIMESTAMPS in USER Model

--- Model/User.js
+++ Model/User.js
@@ -1,28 +1,33 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  Firstname: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    Firstname: {
+      type: String,
+      required: true,
+    },
+    Lastname: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  Lastname: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true, // add createdAT and UpdatedAT field
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
