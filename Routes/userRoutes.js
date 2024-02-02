1339b3c0ae5a56c31051dcbacb57efb421c4b578
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-02 22:18:03 +0530

    Enhanced code readability by separating routes -- USER

---
+++ Routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+const userController = require("../Controllers/userController");
+
+router.post("/register", userController.register);
+
+router.post("/login", userController.login);
+
+router.get("/", userController.getAllUsers);
+
+module.exports = router;
