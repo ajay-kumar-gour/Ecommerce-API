83928dbafbe2c9bb558dbd429ded719ac8b7e271
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-04 21:14:55 +0530

    Moved Database from LOCAL to CLOUD

--- database.js
+++ database.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const Mongo_LOCAL_URL = process.env.Mongo_LOCAL_URL;
-
-mongoose.connect(Mongo_LOCAL_URL);
+const Mongo_CLOUD_URL = process.env.Mongo_CLOUD_URL;
+const dbName = "NODEBAY";
+mongoose.connect(Mongo_CLOUD_URL, {dbName:dbName});
 
 mongoose.connection.on("connected", () => {
   console.log("connceetd to MONGODB..");
