a180141f509a05ed4eb5388af2a177b4d9e60d71
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-04 09:21:02 +0530

    Implemented (TEST) authenticateToken directly on  parent PRDOUCT ENDPOINT

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const dbConnection = require("./database");
 const mongoose = require("mongoose");
-
+const authenticateToken = require("./Middleware/authMiddleware");
 const app = express();
 const PORT = process.env.PORT || 8000; // do not put semicolon on the env file
 
@@ -10,10 +10,9 @@ const userRoutes = require("./Routes/userRoutes");
 const productRoutes = require("./Routes/productRoutes");
 app.use(express.json());
 
-
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
-
+// app.use("/products",authenticateToken, productRoutes);
 
 //a middleware used without any path , it means it will catch all routes for undefined routes.
 app.use((req, res) => {
