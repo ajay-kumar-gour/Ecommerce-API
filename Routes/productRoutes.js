cbcb10770dd9fde2aa5b744559efb8cafe49effb
ajay-kumar-gour <me.akgour@gmail.com>
2024-02-04 09:18:46 +0530

    Implemented authenticateToken on CREATE,UPDATE,DELETE Product

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const productController = require("../Controllers/productController");
+const authenticateToken = require("../Middleware/authMiddleware");
 
 const routes = express.Router();
 
-routes.post("/", productController.createProduct);
+routes.post("/", authenticateToken,productController.createProduct);
 routes.get("/", productController.getAllProducts);
 routes.get("/id/:id", productController.getProductByID);
 routes.get("/name/:name", productController.getProductByName);
 
-routes.put("/id/:id", productController.updateProductByID);
+routes.put("/id/:id",authenticateToken, productController.updateProductByID);
 
-routes.delete("/id/:id", productController.deleteProductByID);
+routes.delete("/id/:id",authenticateToken, productController.deleteProductByID);
 
 module.exports = routes;
